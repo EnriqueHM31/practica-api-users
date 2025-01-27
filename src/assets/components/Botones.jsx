73f2fbab7e97b67992c12c441500b723eabc4e03
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-27 17:15:50 -0600

    Agregar el metodo eliminarUser

--- src/assets/components/Botones.jsx
+++ src/assets/components/Botones.jsx
@@ -1,10 +1,10 @@
 
 
-export default function Botones({ handleFilterPais, handlePintarColumnas }) {
+export default function Botones({ handleFilterPais, handlePintarColumnas, handleResetearFilas }) {
     const BOTONES = [
         { id: 1, texto: "Pintar Columnas", metodo: () => handlePintarColumnas(), },
         { id: 2, texto: "Ordenar por pais", metodo: () => handleFilterPais() },
-        { id: 3, texto: "Resetear Filas" }
+        { id: 3, texto: "Resetear Filas", metodo: () => handleResetearFilas() },
     ]
 
     return (
