5f5dc743d749e12b05f61fb4742b3520f89cfc70
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-27 17:15:30 -0600

    Agregar el metodo eliminarUser

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import TablUser from "./assets/components/TableUsers";
 import Botones from "./assets/components/Botones";
+import { useRef } from "react";
 
 function App() {
   const API_URL = "https://randomuser.me/api/?results=100";
@@ -9,11 +10,16 @@ function App() {
   const [users, setUsers] = useState([]);
   const [columnasPintadas, setColumnasPintadas] = useState(false);
 
+  const originalUser = useRef();
+
   useEffect(() => {
     setTimeout(() => {
       fetch(API_URL)
         .then((response) => response.json())
-        .then((dataUser) => setUsers(dataUser.results));
+        .then((dataUser) => {
+          setUsers(dataUser.results)
+          originalUser.current = dataUser.results;
+        });
     }, 2000)
 
   }, []);
@@ -26,7 +32,15 @@ function App() {
   const handleFilterPais = () => {
     const filtradoPais = [...users].toSorted((a, b) => a.location.country.localeCompare(b.location.country));
     setUsers(filtradoPais);
+  }
 
+  const handleResetearFilas = () => {
+    setUsers(originalUser.current);
+  }
+
+  const handleEliminarUsuario = (userId) => {
+    const eliminado = users.filter(user => user.login.uuid !== userId);
+    setUsers(eliminado);
   }
 
   return (
@@ -35,14 +49,14 @@ function App() {
         <h1>Api Users App</h1>
 
         <div className='contenedor-botones'>
-          <Botones handleFilterPais={handleFilterPais} handlePintarColumnas={handlePintarColumnas} />
+          <Botones handleFilterPais={handleFilterPais} handlePintarColumnas={handlePintarColumnas} handleResetearFilas={handleResetearFilas} />
         </div>
       </header >
 
       <main>
         <h2>Tabla de usuarios de la API Random User</h2>
 
-        {users.length !== 0 ? <TablUser pintarColumnas={columnasPintadas} users={users} /> : <p className="loading">Cargando...</p>}
+        {users.length !== 0 ? <TablUser pintarColumnas={columnasPintadas} users={users} handleEliminarUsuario={handleEliminarUsuario} /> : <p className="loading">Cargando...</p>}
       </main>
     </div >
 
