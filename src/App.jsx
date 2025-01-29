9b74bb42d16c8f11c8f628ddf116019cbccbfa31
EnriqueHM31 <luisenriquehernandezmarin0@gmail.com>
2025-01-29 14:43:52 -0600

    Puse el componente de los botones pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Fragment } from "react";
 import TableUser from "./components/TableUsers";
 import Botones from "./components/Botones";
 import { useObtenerUsuarios } from "./services/obtenerUsuarios";
@@ -6,11 +5,11 @@ import { useBotones } from "./hooks/useBotones";
 import { usePages } from "./hooks/usePages";
 import { useOrdenado } from "./hooks/useOrdenado";
 import { MAX_PAGE } from "./utils/config";
+import BotonesPage from "./components/BotonesPage";
 
 
 
 function App() {
-
 
   const { pagina, handlePagesNext, handlePagesBefore } = usePages();
   const { users, loading, error, handleEliminarUsuario, handleResetearFilas } = useObtenerUsuarios(pagina);
@@ -22,7 +21,7 @@ function App() {
       <header>
         <h1>Api Users App</h1>
 
-        <main className='contenedor-botones'>
+        <div className='contenedor-botones'>
           <Botones
             users={newUser}
             filtrado={filtrado}
@@ -36,32 +35,27 @@ function App() {
             handleResetearFilas={handleResetearFilas}
           />
           <strong className="paginas">Pagina {pagina} / {MAX_PAGE}</strong>
-        </main>
+        </div>
       </header >
 
       <main>
         <h2>Tabla de usuarios de la API Random User</h2>
 
         {error !== null && <p className="error">{error}</p>}
+
         {!loading && error === null && newUser.length > 0
           &&
-          <Fragment>
+          <>
             <TableUser
               users={newUser}
               ordenado={ordenado}
               pintarColumnas={columnasPintadas}
               handleFiltrarHead={handleFiltrarHead}
               handleEliminarUsuario={handleEliminarUsuario} />
-            <div className="contenedor-botones-paginas">
-              <button onClick={() => handlePagesBefore()} className="btn-pages">
-                <img src="src/assets/left.png" alt="imagen del boton paginas anteriores" />
-              </button>
-              <button onClick={() => handlePagesNext()} className="btn-pages">
-                <img src="src/assets/right.png" alt="imagen del boton paginas siguientes" />
-              </button>
-            </div>
-          </Fragment>
+            <BotonesPage pagina={pagina} handlePagesBefore={handlePagesBefore} handlePagesNext={handlePagesNext} />
+          </>
         }
+
         {loading ? error === null ? <p className="loading">Cargando...</p> : null : null}
       </main>
     </div >
